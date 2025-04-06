fd6e8b72fbf3b136c52aad54f71b8a9e8275268a
Maximilian Dittrich <mcldittrich@gmail.com>
2025-04-06 20:47:31 +0200

    successfully reading from db

--- inventory_python/settings.py
+++ inventory_python/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'inventory_python.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'inventory_python.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inventory',
+        'USER': 'root',
+        'PASSWORD': 'MclD2608',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION',
+        }
     }
 }
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Items, Usergroups, Users, Warehouse
 
 # Create your views here.
+
+def kontrolluebersicht(request):
+    items = Items.objects.all()
+    user_groups = Usergroups.objects.all()
+    users = Users.objects.all()
+    warehouses = Warehouse.objects.all()
+
+    context = {
+        'items': items,
+        'user_groups': user_groups,
+        'users': users,
+        'warehouses': warehouses,
+    }
+    return render(request, 'kontrolluebersicht.html', context)
