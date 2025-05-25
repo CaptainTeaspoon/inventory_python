dea041fb28836eb2363af05bc7af9208c6b7dc9c
Maximilian Dittrich <mcldittrich@gmail.com>
2025-05-25 21:11:22 +0200

    Switched from MariaDB to SQLite

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,118 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
 
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
 
 
 class Items(models.Model):
@@ -125,7 +14,7 @@ class Items(models.Model):
     warehouse = models.ForeignKey('Warehouse', models.DO_NOTHING, db_column='Warehouse_ID', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
-        managed = False
+        #managed = False
         db_table = 'items'
 
 
@@ -136,7 +25,7 @@ class Usergroups(models.Model):
     def __str__(self):
         return self.name
     class Meta:
-        managed = False
+        #managed = False
         db_table = 'usergroups'
 
 
@@ -150,7 +39,7 @@ class Users(models.Model):
     usergroup = models.ForeignKey(Usergroups, models.DO_NOTHING, db_column='Usergroup_ID', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
-        managed = False
+        #managed = False
         db_table = 'users'
 
 
@@ -160,7 +49,7 @@ class Warehouse(models.Model):
     usergroup = models.ForeignKey(Usergroups, models.DO_NOTHING, db_column='Usergroup_ID', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
-        managed = False
+        #managed = False
         db_table = 'warehouse'
 
 class UserProfile(models.Model):
@@ -170,7 +59,3 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
     
-    class Meta:
-        managed = False
-        db_table = 'user_profile'
-        unique_together = (('user', 'Usergroup'),)
